a6bc0e2dd316aff2b9e7c7d2fc26eaf2ed708357
real_sohel <salmanisohel26@gmail.com>
2024-01-31 20:03:16 +0530

    Day 41: Insert and Search in a Trie

---
+++ day-40-30Jan24.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// LCS of three strings
+
+// Given 3 strings A, B and C, the task is to find the length of the longest sub-sequence that is common in all the three given strings.
+
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3){
+    // your code here
+    
+}
+
+
+int main(){
+    return 0;
+}
